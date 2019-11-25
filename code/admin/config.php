6b4a5eb168d207a5537908561b82a667f9756272
CAM-DEV006\longdy.sim <longdy.sim@ecoinsoft.com>
2019-11-25 15:47:41 +0700

    update

---
+++ code/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://codedemos.com/admin/');
+define('HTTP_CATALOG', 'http://codedemos.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://codedemos.com/admin/');
+define('HTTPS_CATALOG', 'http://codedemos.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/codedemos/public_html/admin/');
+define('DIR_SYSTEM', '/home/codedemos/public_html/system/');
+define('DIR_IMAGE', '/home/codedemos/public_html/image/');
+define('DIR_LANGUAGE', '/home/codedemos/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home/codedemos/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home/codedemos/public_html/system/config/');
+define('DIR_CACHE', '/home/codedemos/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/codedemos/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/codedemos/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/codedemos/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/codedemos/public_html/system/storage/upload/');
+define('DIR_CATALOG', '/home/codedemos/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'codedemo_brad');
+define('DB_PASSWORD', 'abc123!');
+define('DB_DATABASE', 'codedemo_djoutlet');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ code/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://codedemos.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://codedemos.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/codedemos/public_html/catalog/');
+define('DIR_SYSTEM', '/home/codedemos/public_html/system/');
+define('DIR_IMAGE', '/home/codedemos/public_html/image/');
+define('DIR_LANGUAGE', '/home/codedemos/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/codedemos/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/codedemos/public_html/system/config/');
+define('DIR_CACHE', '/home/codedemos/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/codedemos/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/codedemos/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/codedemos/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/codedemos/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'codedemo_brad');
+define('DB_PASSWORD', 'abc123!');
+define('DB_DATABASE', 'codedemo_djoutlet');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
